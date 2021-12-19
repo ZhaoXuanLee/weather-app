625a4af0d8b37be7295542576ba3260a9a70b3fb
ZhaoXuanLee <zhaoxuanlee19@gmail.com>
2021-12-19 02:43:17 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+    let location="";
+    let temperature="";
+    let description="";
+</script>
+<h1>Welcome to SvelteKit</h1>
+<form on:submit|preventDefault={getWeather}>
+    <label for="location">Location:</label>
+    <input id="location" bind:value={location} type="text" />
+    <button type="submit">Check</button>
+</form>
